a784597276d15b8ad1ca8a78388029bace92ab12
Giulia Bianchi <gbianchi@xebia.fr>
2019-11-06 16:38:10 +0100

    update model.py and task.py for first test

--- edml-trainer/trainer/model.py
+++ edml-trainer/trainer/model.py
@@ -1,67 +1,78 @@
 import tensorflow as tf
-import numpy as np
-import shutil
-print(tf.__version__)
 
 # Read data
+BUCKET = None
+PATTERN = "*"
 
 CSV_COLUMNS = ["dayofweek", "hourofday", "pickup_borough", "dropoff_borough", "trip_duration"]
 LABEL_COLUMN = "trip_duration"
+
+# Set default values for each CSV column
 DEFAULTS = [[1], [0], [""], [""],  []]
+
+# Define some hyperparameters
+TRAIN_STEPS = 10000
+EVAL_STEPS = None
+BATCH_SIZE = 512
+NEMBEDS = 3
+NNSIZE = [64, 16, 4]
+
+TROTTLE_SECS = None
+EVAL_DELAY_SECS = None
+
+VERBOSITY = 'INFO'
+
+tf.logging.set_verbosity(v=VERBOSITY)
+
 
 # Create an input function reading a file using the Dataset API
 # Then provide the results to the Estimator API
-def read_dataset(filename, mode, batch_size = 512):
+def read_dataset(prefix, mode, batch_size):
     def _input_fn():
         def decode_csv(records):
             columns = tf.decode_csv(records, record_defaults=DEFAULTS)
             features = dict(zip(CSV_COLUMNS, columns))
-            features["dayofweek"] = features["dayofweek"] - 1
+            features["dayofweek"] -= 1
             label = features.pop(LABEL_COLUMN)
             return features, label
+
+        # filename
+        file_name = '{}/{}/tlc_yellow_trips_2018-{}.csv'.format(BUCKET, prefix, PATTERN)
         
         # Create list of files that match pattern
-        file_list = tf.gfile.Glob(filename)
+        file_list = tf.gfile.Glob(file_name)
 
         # Create dataset from file list
-        dataset = (tf.data.TextLineDataset(file_list,compression_type="GZIP")  # Read text file
+        dataset = (tf.data.TextLineDataset(file_list, compression_type="GZIP")  # Read text file
                    .map(decode_csv))  # Transform each elem by applying decode_csv fn
 
         if mode == tf.estimator.ModeKeys.TRAIN:
-            num_epochs = 5 # None # indefinitely
-            dataset = dataset.shuffle(buffer_size=10*batch_size, seed=42)
+            num_epochs = None  # indefinitely
+            dataset = dataset.shuffle(buffer_size=10*batch_size, seed=1234)
         else:
-            num_epochs = 1 # end-of-input after this
+            num_epochs = 1  # end-of-input after this
 
         dataset = dataset.repeat(num_epochs).batch(batch_size)
         return dataset
     return _input_fn
 
-def get_train_input_fn():
-    return read_dataset('gs://edml/data/taxi-trips/train/tlc_yellow_trips_2018-000*.csv',
-                        mode = tf.estimator.ModeKeys.TRAIN)
-
-def get_valid_input_fn():
-    return read_dataset('gs://edml/data/taxi-trips/val/tlc_yellow_trips_2018-000*.csv',
-                        mode = tf.estimator.ModeKeys.EVAL)
 
 # Feature engineering
-
 def get_wide_deep():
     
     borough_list = ["Manhattan", "Queens", "Brooklyn", "Bronx", "Staten Island", "EWR"]
         
     # One hot encode categorical features
-    fc_dayofweek = tf.feature_column.categorical_column_with_identity(key="dayofweek", num_buckets = 7)
-    fc_hourofday = tf.feature_column.categorical_column_with_identity(key="hourofday", num_buckets = 24)
+    fc_dayofweek = tf.feature_column.categorical_column_with_identity(key="dayofweek", num_buckets=7)
+    fc_hourofday = tf.feature_column.categorical_column_with_identity(key="hourofday", num_buckets=24)
     fc_pickuploc = tf.feature_column.categorical_column_with_vocabulary_list(key="pickup_borough", 
                                                                              vocabulary_list=borough_list)
     fc_dropoffloc = tf.feature_column.categorical_column_with_vocabulary_list(key="dropoff_borough", 
                                                                               vocabulary_list=borough_list)
     
     # Cross features to get combination of day and hour and pickup-dropoff locations
-    fc_crossed_day_hr = tf.feature_column.crossed_column(keys = [fc_dayofweek, fc_hourofday], hash_bucket_size = 24 * 7)
-    fc_crossed_pd_pair = tf.feature_column.crossed_column(keys = [fc_pickuploc, fc_dropoffloc], hash_bucket_size = 6*6)
+    fc_crossed_day_hr = tf.feature_column.crossed_column(keys=[fc_dayofweek, fc_hourofday], hash_bucket_size=24*7)
+    fc_crossed_pd_pair = tf.feature_column.crossed_column(keys=[fc_pickuploc, fc_dropoffloc], hash_bucket_size=6*6)
     
     wide = [
         # Feature crosses
@@ -73,8 +84,8 @@ def get_wide_deep():
     ]
     
     # Embedding_column to "group" together ...
-    fc_embed_pd_pair = tf.feature_column.embedding_column(categorical_column = fc_crossed_pd_pair, dimension = 4)
-    fc_embed_day_hr = tf.feature_column.embedding_column(categorical_column = fc_crossed_day_hr, dimension = 16)
+    fc_embed_pd_pair = tf.feature_column.embedding_column(categorical_column=fc_crossed_pd_pair, dimension=NEMBEDS)
+    fc_embed_day_hr = tf.feature_column.embedding_column(categorical_column=fc_crossed_day_hr, dimension=NEMBEDS)
     
     deep = [
         fc_embed_pd_pair,
@@ -83,14 +94,14 @@ def get_wide_deep():
     
     return wide, deep
 
-# Serving input reciver function
 
+# Serving input receiver function
 def serving_input_receiver_fn():
     receiver_tensors = {
-        'dayofweek' : tf.placeholder(dtype = tf.int64, shape = [None], name="dayofweek"),
-        'hourofday' : tf.placeholder(dtype = tf.int64, shape = [None], name="hourofday"),
-        'pickup_borough' : tf.placeholder(dtype = tf.string, shape = [None], name="pickup_borough"), 
-        'dropoff_borough' : tf.placeholder(dtype = tf.string, shape = [None], name="dropoff_borough"),
+        'dayofweek': tf.placeholder(dtype=tf.int64, shape=[None], name="dayofweek"),
+        'hourofday': tf.placeholder(dtype=tf.int64, shape=[None], name="hourofday"),
+        'pickup_borough': tf.placeholder(dtype=tf.string, shape=[None], name="pickup_borough"),
+        'dropoff_borough': tf.placeholder(dtype=tf.string, shape=[None], name="dropoff_borough"),
     }
     
     features = {
@@ -100,51 +111,43 @@ def serving_input_receiver_fn():
         
     return tf.estimator.export.ServingInputReceiver(features = features, receiver_tensors = receiver_tensors)
 
-# Build and train model
 
+# Build and train model
 # Create estimator to train and evaluate
 def train_and_evaluate(output_dir):
-    
-    EVAL_INTERVAL = 300
+    tf.summary.FileWriterCache.clear() # ensure filewriter cache is clear for TensorBoard events file
+
     wide, deep = get_wide_deep()
     
-    run_config = tf.estimator.RunConfig(save_checkpoints_secs = EVAL_INTERVAL,
-                                        tf_random_seed = 2810,
-                                        keep_checkpoint_max = 3)
+    run_config = tf.estimator.RunConfig(save_checkpoints_secs=TROTTLE_SECS,
+                                        tf_random_seed=2810,
+                                        keep_checkpoint_max=3)
     
     # Add custom evaluation metric
     def my_rmse(labels, predictions):
-        pred_values = tf.squeeze(input = predictions["predictions"], axis = -1)
-        return {"rmse": tf.metrics.root_mean_squared_error(labels = labels, predictions = pred_values)}
+        pred_values = tf.squeeze(input=predictions["predictions"], axis=-1)
+        return {"rmse": tf.metrics.root_mean_squared_error(labels=labels, predictions=pred_values)}
     
     estimator = tf.estimator.DNNLinearCombinedRegressor(
-        model_dir = output_dir,
-        linear_feature_columns = wide,
-        dnn_feature_columns = deep,
-        dnn_hidden_units = [128, 64, 32],
-        config = run_config)
+        model_dir=output_dir,
+        linear_feature_columns=wide,
+        dnn_feature_columns=deep,
+        dnn_hidden_units=NNSIZE,
+        config=run_config)
     
-    estimator = tf.contrib.estimator.add_metrics(estimator = estimator, metric_fn = my_rmse) 
+    estimator = tf.contrib.estimator.add_metrics(estimator=estimator, metric_fn=my_rmse)
     
     train_spec = tf.estimator.TrainSpec(
-        input_fn = get_train_input_fn(),
-        max_steps = 500)
+        input_fn=read_dataset('train', tf.estimator.ModeKeys.TRAIN, BATCH_SIZE),
+        max_steps=TRAIN_STEPS)
     
-    exporter = tf.estimator.LatestExporter('exporter', serving_input_receiver_fn = serving_input_receiver_fn)
+    exporter = tf.estimator.LatestExporter('exporter', serving_input_receiver_fn=serving_input_receiver_fn)
     
     eval_spec = tf.estimator.EvalSpec(
-        input_fn = get_valid_input_fn(),
-        steps = None,
-        start_delay_secs = 60, # start evaluating after N seconds
-        throttle_secs = EVAL_INTERVAL,  # evaluate every N seconds
-        exporters = exporter)
+        input_fn=read_dataset('val', tf.estimator.ModeKeys.EVAL, 2**15),
+        steps=EVAL_STEPS,
+        start_delay_secs=EVAL_DELAY_SECS,  # start evaluating after N seconds
+        throttle_secs=TROTTLE_SECS,  # evaluate every N seconds
+        exporters=exporter)
     
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
-    
-# Train model
-
-OUTDIR = "gs://edml/data/taxi-trips/model"
-# gsutil rm gs://edml/data/taxi-trips/model** # start fresh each time
-tf.summary.FileWriterCache.clear() # ensure filewriter cache is clear for TensorBoard events file
-tf.logging.set_verbosity(v = tf.logging.INFO) # so loss is printed during training
-
--- edml-trainer/trainer/task.py
+++ edml-trainer/trainer/task.py
@@ -1,88 +1,91 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Example implementation of code to run on the Cloud ML service.
-"""
-
 import argparse
-import json
-import os
 
 from . import model
 
+
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
+
     # Input Arguments
     parser.add_argument(
-        '--train_data_paths',
-        help = 'GCS or local path to training data',
-        required = True
+        '--bucket',
+        help='GCS path to project bucket',
+        required=True
     )
-    parser.add_argument(
-        '--train_batch_size',
-        help = 'Batch size for training steps',
-        type = int,
-        default = 512
-    )
-    parser.add_argument(
-        '--train_steps',
-        help = 'Steps to run the training job for',
-        type = int
-    )
-    parser.add_argument(
-        '--eval_steps',
-        help = 'Number of steps to run evalution for at each checkpoint',
-        default = 10,
-        type = int
-    )
-    parser.add_argument(
-        '--eval_data_paths',
-        help = 'GCS or local path to evaluation data',
-        required = True
-    )
-    # Training arguments
-    parser.add_argument(
-        '--hidden_units',
-        help = 'List of hidden layer sizes to use for DNN feature columns',
-        nargs = '+',
-        type = int,
-        default = [128, 32, 4]
-    )
+
     parser.add_argument(
         '--output_dir',
-        help = 'GCS location to write checkpoints and export models',
-        required = True
+        help='GCS location to write checkpoints and export models',
+        required=True
+    )
+
+    parser.add_argument(
+        '--pattern',
+        help='Pattern to read file partitions',
+        default='*'
     )
     parser.add_argument(
+        '--batch-size',
+        help='Number of examples to compute gradient over',
+        type=int,
+        default=512
+    )
+
+    parser.add_argument(
+        '--train_examples',
+        help='Number of examples (in thousands) to run the training job over. '
+             'If this is more than actual # of examples available, it cycles through them. '
+             'So specifying 1000 here when you have only 100k examples makes this 10 epochs.',
+        type=int,
+        default=435000
+    )
+
+    parser.add_argument(
+        '--eval-steps',
+        help='Positive number of steps for which to evaluate model. '
+             'Default to None, which means to evaluate until input_fn raises an end-of-input exception',
+        default=None,
+        type=int
+    )
+    parser.add_argument(
+        '--nnsize',
+        help='Hidden layer sizes to use for DNN feature columns -- provide space-separated layers',
+        nargs='+',
+        type=int,
+        default=[128, 32, 4]
+    )
+    parser.add_argument(
+        '--nembeds',
+        help='Embedding size of a cross of n key real-valued parameters',
+        type=int,
+        default=3
+    )
+
+    parser.add_argument(
         '--job-dir',
-        help = 'this model ignores this field, but it is required by gcloud',
-        default = 'junk'
+        help='this model ignores this field, but it is required by gcloud',
+        default='junk'
     )
 
     # Eval arguments
     parser.add_argument(
-        '--eval_delay_secs',
-        help = 'How long to wait before running first evaluation',
-        default = 10,
-        type = int
+        '--eval-delay-secs',
+        help='How long to wait before running first evaluation',
+        default=10,
+        type=int
     )
     parser.add_argument(
-        '--throttle_secs',
-        help = 'Seconds between evaluations',
-        default = 300,
-        type = int
+        '--throttle-secs',
+        help='Seconds between evaluations',
+        default=300,
+        type=int
     )
+
+    parser.add_argument(
+        '--verbosity',
+        choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+        default='INFO')
 
     args = parser.parse_args()
     arguments = args.__dict__
@@ -91,15 +94,30 @@ if __name__ == '__main__':
     arguments.pop('job_dir', None)
     arguments.pop('job-dir', None)
 
-    output_dir = arguments['output_dir']
-    # Append trial_id to path if we are doing hptuning
-    # This code can be removed if you are not using hyperparameter tuning
-    output_dir = os.path.join(
-        output_dir,
-        json.loads(
-            os.environ.get('TF_CONFIG', '{}')
-        ).get('task', {}).get('trail', '')
-    )
+    # output_dir = arguments['output_dir']
+    # # Append trial_id to path if we are doing hptuning
+    # # This code can be removed if you are not using hyperparameter tuning
+    # output_dir = os.path.join(
+    #     output_dir,
+    #     json.loads(
+    #         os.environ.get('TF_CONFIG', '{}')
+    #     ).get('task', {}).get('trail', '')
+    # )
+
+    # assign the arguments to the model variables
+    output_dir = arguments.pop('output_dir')
+    model.BUCKET = arguments.pop('bucket')
+    model.BATCH_SIZE = arguments.pop('batch_size')
+    model.TRAIN_STEPS = (arguments.pop('train_examples') * 1000) / model.BATCH_SIZE
+    model.EVAL_STEPS = arguments.pop('eval_steps')
+    print("Will train for {} steps using batch_size={}".format(model.TRAIN_STEPS, model.BATCH_SIZE))
+    model.PATTERN = arguments.pop('pattern')
+    model.NEMBEDS = arguments.pop('nembeds')
+    model.NNSIZE = arguments.pop('nnsize')
+    print("Will use DNN size of {}".format(model.NNSIZE))
+    model.TROTTLE_SECS = arguments.pop('throttle_secs')
+    model.EVAL_DELAY_SECS = arguments.pop('eval_delay_secs')
+    model.VERBOSITY = arguments.pop('verbosity')
 
     # Run the training job
-    model.train_and_evaluate(arguments)
+    model.train_and_evaluate(output_dir)
