da460e5aadfa9dc5dedb1bfda5c6e4719042f261
giulbia <gbianchi@xebia.fr>
2019-11-08 17:45:29 +0000

    push files

---
+++ edml-notebooks/gbi/submit-training.sh
@@ -0,0 +1,26 @@
+BUCKET=edml
+echo $BUCKET
+TRAINER_PACKAGE_PATH=gs://${BUCKET}/data/taxi-trips/sources
+echo $TRAINER_PACKAGE_PATH
+MAIN_TRAINER_MODULE="trainer.task"
+PACKAGE_STAGING_PATH=gs://${BUCKET}/data/taxi-trips/staging
+echo $PACKAGE_STAGING_PATH
+now=$(date +"%Y%m%d")
+JOB_NAME=edml_trainer_$now
+JOB_DIR=gs://${BUCKET}/data/taxi-trips/${JOB_NAME}
+REGION="europe-west1-b"
+OUTDIR=gs://${BUCKET}/data/taxi-trips/model_test
+# gsutil -m rm -rf $OUTDIR
+
+gcloud ai-platform jobs submit training $JOB_NAME \
+    --staging-bucket $PACKAGE_STAGING_PATH \
+    --job-dir $JOB_DIR \
+    --package-path $TRAINER_PACKAGE_PATH \
+    --module-name $MAIN_TRAINER_MODULE \
+    --region $REGION \
+    -- \
+    --bucket=${BUCKET} \
+    --output-dir=${OUTDIR} \
+    --pattern="*" \
+    --train-examples=500 \
+    --eval-steps=1
