324ed899446dd7383f9f631e7627fd1b9000e017
Loïc DIVAD <ldivad@isep.fr>
2019-11-23 11:58:40 +0100

    Ai Platform jobs submission manual (#12)
    * remove outliers: keep trip duration <=180 min
    
    * test new features and shuffle buffer size
    
    * test shuffle buffer size and eval-steps
    
    * change buffer_size and eval-steps
    
    * update model and submit-training to train remotly.--train-examples=500 buffer_size=100*batch_size
    
    * train on full data and shuffle size augmented. works fine

--- edml-notebooks/gbi/submit-training.sh
+++ edml-notebooks/gbi/submit-training.sh
@@ -3,23 +3,27 @@
 BUCKET=edml
 echo $BUCKET
 
-TRAINER_PACKAGE_PATH=gs://$BUCKET/data/taxi-trips/sources
+TRAINER_PACKAGE_PATH=edml-trainer/trainer
 echo $TRAINER_PACKAGE_PATH
 
 MAIN_TRAINER_MODULE="trainer.task"
 
-PACKAGE_STAGING_PATH=gs://$BUCKET/data/taxi-trips/staging
+PACKAGE_STAGING_PATH=gs://$BUCKET
 echo $PACKAGE_STAGING_PATH
 
-now=$(date +"%Y%m%d")
+now=$(date +"%y%m%d_%H%M%S")
 JOB_NAME=edml_trainer_$now
 echo $JOB_NAME
 
-JOB_DIR=gs://$BUCKET/data/taxi-trips/$JOB_NAME
-REGION="europe-west1-b"
+JOB_DIR=gs://$BUCKET/$JOB_NAME/job_dir
+REGION="europe-west1"
 
-OUTDIR=gs://$BUCKET/data/taxi-trips/model_test
-gsutil -m rm -rf $OUTDIR
+PYTHON_VERSION=3.5
+RUNTIME_VERSION=1.14
+
+OUTDIR=gs://$BUCKET/$JOB_NAME/model
+echo $OUTDIR
+#gsutil -m rm -rf $OUTDIR
 
 gcloud ai-platform jobs submit training $JOB_NAME \
     --staging-bucket $PACKAGE_STAGING_PATH \
@@ -27,9 +31,13 @@ gcloud ai-platform jobs submit training $JOB_NAME \
     --package-path $TRAINER_PACKAGE_PATH \
     --module-name $MAIN_TRAINER_MODULE \
     --region $REGION \
+    --python-version $PYTHON_VERSION \
+    --runtime-version $RUNTIME_VERSION \
     -- \
     --bucket=$BUCKET \
     --output-dir=$OUTDIR \
     --pattern="*" \
-    --train-examples=500 \
+    --nembeds 10 \
+    --nnsize 10 5 \
+    --train-examples=174000 \
     --eval-steps=1
