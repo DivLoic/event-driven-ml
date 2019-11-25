3035058fb4ed0f8166f99960487002910d1eee76
Giulia Bianchi <gbianchi@xebia.fr>
2019-11-25 20:47:35 +0100

    change parameters for v2 to improve overfit

--- edml-notebooks/gbi/submit-training.sh
+++ edml-notebooks/gbi/submit-training.sh
@@ -8,25 +8,22 @@ echo $TRAINER_PACKAGE_PATH
 
 MAIN_TRAINER_MODULE="trainer.task"
 
-PACKAGE_STAGING_PATH=gs://$BUCKET
-echo $PACKAGE_STAGING_PATH
-
-now=$(date +"%y%m%d_%H%M%S")
+now=$(date +"%Y%m%d_%H%M%S")
 JOB_NAME=edml_trainer_$now
 echo $JOB_NAME
 
-JOB_DIR=gs://$BUCKET/$JOB_NAME/job_dir
+JOB_DIR=gs://$BUCKET/ai-platform/$JOB_NAME
+echo $JOB_DIR
 REGION="europe-west1"
 
 PYTHON_VERSION=3.5
 RUNTIME_VERSION=1.14
 
-OUTDIR=gs://$BUCKET/$JOB_NAME/model
+OUTDIR=gs://$BUCKET/ai-platform/models
 echo $OUTDIR
 #gsutil -m rm -rf $OUTDIR
 
 gcloud ai-platform jobs submit training $JOB_NAME \
-    --staging-bucket $PACKAGE_STAGING_PATH \
     --job-dir $JOB_DIR \
     --package-path $TRAINER_PACKAGE_PATH \
     --module-name $MAIN_TRAINER_MODULE \
@@ -38,6 +35,7 @@ gcloud ai-platform jobs submit training $JOB_NAME \
     --output-dir=$OUTDIR \
     --pattern="*" \
     --nembeds 10 \
-    --nnsize 10 5 \
+    --nnsize 20 10 5 \
     --train-examples=174000 \
-    --eval-steps=1
+    --eval-steps=1 \
+    --batch-size=128
