5eee941aa5111e33ee0afc88b3eb9f80da5b0782
DivLoic <ldivad@isep.fr>
2020-04-07 15:20:41 +0200

    feature - refactor the ksql module

--- terraform/modules/ksql/data.tf
+++ terraform/modules/ksql/data.tf
@@ -4,19 +4,17 @@ data "template_file" "ksql_server_properties" {
 
   vars = {
 
-    global_prefix = "${var.global_prefix}"
-    broker_list = "${var.ccloud_broker_list}"
-    access_key = "${var.ccloud_access_key}"
-    secret_key = "${var.ccloud_secret_key}"
-    sr_access_key = "${var.ccloud_sr_access_key}"
-    sr_secret_key = "${var.ccloud_sr_secret_key}"
-    confluent_home_value = "${var.confluent_home_value}"
+    global_prefix        = var.global_prefix
+    broker_list          = var.confluent_cloud_conf["broker_list"]
+    access_key           = var.confluent_cloud_conf["access_key"]
+    secret_key           = var.confluent_cloud_conf["secret_key"]
+    sr_access_key        = var.confluent_cloud_conf["sr_access_key"]
+    sr_secret_key        = var.confluent_cloud_conf["sr_secret_key"]
+    confluent_home_value = var.confluent_home_value
 
-    schema_registry_url = "${var.ccloud_schema_registry_url}"
-    schema_registry_basic_auth = "${var.ccloud_schema_registry_basic_auth}"
-
+    schema_registry_url        = var.confluent_cloud_conf["schema_registry_url"]
+    schema_registry_basic_auth = var.confluent_cloud_conf["schema_registry_basic_auth"]
   }
-
 }
 
 data "template_file" "ksql_server_bootstrap" {
@@ -25,10 +23,8 @@ data "template_file" "ksql_server_bootstrap" {
 
   vars = {
 
-    confluent_platform_location = "${var.confluent_platform_location}"
-    ksql_server_properties = "${data.template_file.ksql_server_properties.rendered}"
-    confluent_home_value = "${var.confluent_home_value}"
-
+    confluent_home_value        = var.confluent_home_value
+    confluent_platform_location = var.confluent_platform_location
+    ksql_server_properties      = data.template_file.ksql_server_properties.rendered
   }
-
 }
