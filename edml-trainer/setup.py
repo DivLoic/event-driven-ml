fa506666944acff39f0359bbcf7c03928bc6e92a
DivLoic <ldivad@isep.fr>
2020-04-11 22:05:52 +0200

    move the build from gradle to maven

--- edml-trainer/setup.py
+++ edml-trainer/setup.py
@@ -1,19 +1,12 @@
 from distutils.core import setup
 from setuptools import find_packages
-import configparser
-
+import os
 
 if __name__ == '__main__':
 
-    with open('../build/version.properties', 'r', encoding="utf-8") as f:
-        config_string = '[root]\n' + f.read()
-
-    config = configparser.ConfigParser()
-    config.read_string(config_string, 'utf-8')
-
     setup(
         name="edml-trainer",
-        version=config['root']['VERSION_DISPLAY'],
+        version=os.getenv('version', 'SNAPSHOT'),
         description='Predict trip duration - NYC taxi public dataset',
         author='gbianchi',
         author_email='gbianchi@xebia.fr',
