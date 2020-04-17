126b464f53d3ca68a839c2701621ba7917285a51
Giulia Bianchi <gbianchi@zebia.fr>
2020-04-17 07:44:23 +0000

    remove parameter train_examples

--- edml-trainer/trainer/task.py
+++ edml-trainer/trainer/task.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import tensorflow as tf
 
 from . import model
@@ -20,36 +21,57 @@ def parse_arguments():
         required=True
     )
 
-    parser.add_argument(
-        '--batch-size',
-        help='Number of examples to compute gradient over',
-        type=int,
-        default=128
-    )
-
-    parser.add_argument(
-        '--train-size',
-        help='Number of examples (in thousands) to run the training job over. '
-             'If this is more than actual # of examples available, it cycles through them. '
-             'So specifying 1000 here when you have only 100k examples makes this 10 epochs.',
-        type=int,
-        default=435000 #174000
-    )
-
-    parser.add_argument(
-        '--eval-steps',
-        help='Positive number of steps for which to evaluate model. '
-             'Default to None, which means to evaluate until input_fn raises an end-of-input exception',
-        default=None,
-        type=int
-    )
-    
-    
+    # Experiment arguments
     parser.add_argument(
         '--train-steps',
-        help='TODO',
-        default=10000,
-        type=int
+        help="""
+        Steps to run the training job for.
+        If --num-epochs and --train-size are not specified,
+        this must be specified;
+        otherwise the training job will run indefinitely.
+        if --num-epochs and --train-size are specified,
+        then --train-steps will default to:
+            (train-size/train-batch-size) * num-epochs
+        """,
+        default=500,
+        type=int)
+    
+    parser.add_argument(
+        '--eval-steps',
+        help="""
+        Number of steps to run evaluation for at each checkpoint.,
+        Set to None to evaluate on the whole evaluation data.
+        """,
+        default=None,
+        type=int)
+    
+    parser.add_argument(
+        '--batch-size',
+        help='Batch size for each training and evaluation step.',
+        type=int,
+        default=128)
+    
+    parser.add_argument(
+        '--train-size',
+        help="""
+        Size of the training data (instance count).
+        If both --train-size and --num-epochs are specified,
+        --train-steps will default to:
+            (train-size/train-batch-size) * num-epochs.
+        """,
+        type=int,
+        default=None)
+    
+    parser.add_argument(
+        '--num-epochs',
+        help="""
+        Maximum number of training data epochs on which to train.
+        If both --train-size and --num-epochs are specified,
+        --train-steps will default to:
+            (train-size/train-batch-size) * num-epochs.
+        """,
+        default=3,
+        type=int,
     )
     
     parser.add_argument(
@@ -68,8 +90,8 @@ def parse_arguments():
 
     parser.add_argument(
         '--job-dir',
-        help='this model ignores this field, but it is required by gcloud',
-        default='junk'
+        help="",
+        default=""
     )
 
     # Eval arguments
@@ -101,6 +123,19 @@ def parse_arguments():
 #             os.environ.get('TF_CONFIG', '{}')
 #         ).get('task', {}).get('trail', '')
 #     )
+
+
+    # Compute the number of Training steps.
+    if args.train_size is not None and args.num_epochs is not None:
+        args.train_steps = int(
+            (args.train_size / args.batch_size) * args.num_epochs)
+    else:
+        args.train_steps = args.train_steps
+
+    logging.info('Training steps: {} ({}).'.format(
+        args.train_steps,
+        'supplied' if args.train_size is None else 'computed'))
+    logging.info('Evaluate every {} steps.'.format(args.eval_delay_secs))
     
     return args
 
@@ -108,7 +143,7 @@ def train_and_evaluate(args):
     
     tf.compat.v1.summary.FileWriterCache.clear()
     
-    estimator, train_spec, eval_spec = model.my_estimator(args.output_dir, args.throttle_secs, args.nnsize, args.batch_size, args.train_steps, args.eval_steps, args.eval_delay_secs)
+    estimator, train_spec, eval_spec = model.my_estimator(args.output_dir, args.throttle_secs, args.nnsize, args.batch_size, args.train_steps, args.eval_steps, args.eval_delay_secs, args.nembeds)
     
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
@@ -118,7 +153,3 @@ if __name__ == '__main__':
     args = parse_arguments()
     tf.compat.v1.logging.set_verbosity(args.verbosity)
     train_and_evaluate(args)
-
-    
-
-    
